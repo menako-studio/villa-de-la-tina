7e77c0fa5521f26b168d96e18d057b6b729baf5c
adibwafi <adibwafi@gmail.com>
2026-01-02 18:34:41 +0700

    fix: page training slug

--- src/pages/training/[slug].tsx
+++ src/pages/training/[slug].tsx
@@ -14,6 +14,7 @@ import {
   HeartIcon,
   UserIcon
 } from '@heroicons/react/24/outline';
+import { Header } from '@/components';
 import { format } from 'date-fns';
 import { id } from 'date-fns/locale';
 import { Training } from '@/types';
